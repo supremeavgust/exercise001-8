8b00e1241890217f0b214099103299f69e827764
Stanislav <avgustwot@mail.ru>
2023-03-11 20:36:29 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2
+
+Console.Clear();
+Console.Write("BBедите число:  ");
+int a =Convert.ToInt32(Console.ReadLine());
+
+Console.Write("BBедите число:  ");
+int b =Convert.ToInt32(Console.ReadLine());
+int max;
+
+  if (a > b)
+            {
+                Console.WriteLine($"Максимальное значение {max=a}");
+                Console.WriteLine($"Минимальное значение {b}");               
+               
+            }
+            else 
+            {
+                Console.WriteLine($"Максимальное значение {max = b}");
+                Console.WriteLine($"Минимальное значение {a}");      
+            }
